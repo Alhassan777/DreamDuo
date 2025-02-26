9374812034b26aa17a6545f9d8be891be5b04f64
Alhassan777 <alhassan@uni.minerva.edu>
2025-02-26 01:54:12 -0800

    Removed the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DailyTasksPage from './pages/DailyTasksPage';
@@ -17,8 +16,7 @@ function App() {
         <ChakraProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/daily-tasks" element={<DailyTasksPage />} />
             <Route path="/edit-profile" element={<EditProfilePage />} />
